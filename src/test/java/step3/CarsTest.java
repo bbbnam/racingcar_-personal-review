5125541002249a03e1bfb4b0b0a5bee7e6ad0a39
bbbnam <bbbnam@naver.com>
2020-11-30 11:51:18 +0900

    test : car의 일급 컬렉션인 cars 객체 테스트

--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -4,17 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarsTest {
 
     @DisplayName("car 일급 콜렉션 객체 cars 생성")
     @Test
     void create() {
-//        Cars cars = new Cars(new Car(0), new Car(0), new Car(0));
-//        assertThat(cars).isEqualTo(new Cars(new Car(0), new Car(0), new Car(0)))
-        // 이렇게 객체를 일일이 생성할 필요 ? -> X
-        // cars 객체 내에서 생성해서 넣어줘도 될듯? 굳이 바깥에서 car 객체를 알 필요는 없는듯..
-        // Cars 객체가 car 객체의 일급 컬렉션이라는 것만 알면 Car 객체를 바깥에서 알 필요 없음.
+        Cars cars = Cars.ValueOf(3);
+        assertThat(cars.getCarCount()).isEqualTo(3);
     }
 }
