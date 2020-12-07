e078e62a5e8e14cc1a5a7c4e570ade863ab8ce54
bbbnam <bbbnam@naver.com>
2020-12-07 12:40:19 +0900

    test : 레이싱 게임 진행 객체 테스트 코드 작성

---
+++ src/test/java/step3/RacingGameTest.java
@@ -0,0 +1,24 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameTest {
+
+    @DisplayName("RacingGame 객체 생성 테스트")
+    @Test
+    void create() {
+        RacingGame racingGame = new RacingGame(3, Cars.ValueOf(3));
+        assertThat(racingGame).isEqualTo(new RacingGame(3, Cars.ValueOf(3)));
+    }
+
+    @DisplayName("시도 횟수에 따라 결과값 진행 테스트")
+    @Test
+    void getResult() {
+        RacingGame racingGame = new RacingGame(5, Cars.ValueOf(3));
+        Result result = racingGame.progress();
+        assertThat(result.getResultSize()).isEqualTo(5);
+    }
+}
