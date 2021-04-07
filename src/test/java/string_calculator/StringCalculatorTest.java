e00dc9ac33410d28d94719bbec319b8dc11a1642
bbbnam <bbbnam@naver.com>
2021-04-07 23:56:33 +0900

    test : 덧셈 테스트 코드 작성

---
+++ src/test/java/string_calculator/StringCalculatorTest.java
@@ -0,0 +1,22 @@
+package string_calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @DisplayName("덧셈")
+    @Test
+    void addTest() {
+        //given
+        String input = "2 + 3";
+
+        //when
+        StringCalculator stringCalculator = new StringCalculator(input);
+        int result = stringCalculator.calculate();
+
+        //then
+        Assertions.assertThat(result).isEqualTo(5);
+    }
+}
