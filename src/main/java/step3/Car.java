b0c630701767303b3aa372a686481649489dd68a
bbbnam <bbbnam@naver.com>
2021-04-14 00:49:21 +0900

    refactor : 자동차 입력값 수정

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,17 +1,33 @@
 package step3;
 
+import java.util.Objects;
+
 public class Car {
 
     private Position position;
 
-    public Car(Position position) {
-        this.position = position;
+    public Car(int position) {
+        this.position = new Position(position);
+
     }
 
-    public Position move(MoveCondition condition) {
+    public Car move(MoveCondition condition) {
         /*if (condition.isMovable()) {
 
         }*/
-        return new Position(1);
+        return new Car(1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
